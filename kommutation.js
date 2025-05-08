1749cb71dc6d3d22963f47925d5e13959f50a7da
Oliver Blanthorn <freedom4cows@gmail.com>
2025-05-08 11:44:41 +0200

    Initial commit

---
+++ kommutation.js
@@ -0,0 +1,81 @@
+// ==UserScript==
+// @name         Kommutation
+// @namespace    http://tampermonkey.net/
+// @version      2025-05-08
+// @description  grab a gpx file off komoot - edit the route, reload the page, click the button
+// @author       bovine3dom
+// @match        https://www.komoot.com/plan/tour/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// @run-at       document-end
+// ==/UserScript==
+
+(function() {
+    // 1) go on a route
+    // 2) click edit
+    // 3) reload the page (important!) with e.g. ctrl+r
+    // 4) click the Grab GPX button
+
+    // following adapted lightly from
+    // https://github.com/DreiDe/komootGPXport
+    function jsonToGpx(coords) {
+        let gpx =
+            `<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
+                <gpx version="1.1" creator="komootGPXport">
+                  <metadata></metadata>
+                    <rte>
+                      ${coords.map((coord) => {
+                          return `<rtept lat="${coord.lat}" lon="${coord.lng}"><ele>${coord.z}</ele></rtept>`
+                      }).join('\n')}
+                    </rte>
+                </gpx>`
+        return gpx
+    }
+
+    function downloadGpx(filename, text){
+        let elem = document.createElement('a')
+        elem.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+        elem.setAttribute('download', filename)
+
+        elem.style.display = 'none'
+        document.body.appendChild(elem)
+
+        elem.click()
+
+        document.body.removeChild(elem)
+    }
+    // adaptation ends
+
+    const grabbutton = document.createElement("button");
+    grabbutton.style.position = "fixed"
+    grabbutton.style.top = "10px"
+    grabbutton.style.left = "10px"
+    grabbutton.style.zIndex = "2147483647"
+    grabbutton.style.padding = "10px 15px"
+    grabbutton.style.backgroundColor = "lightgreen"
+    grabbutton.style.color = "black"
+    grabbutton.style.border = "2px solid darkgreen"
+    grabbutton.style.borderRadius = "5px"
+    grabbutton.style.cursor = "pointer"
+    grabbutton.style.fontSize = "14px"
+    grabbutton.style.setProperty("display", "block", "important")
+    grabbutton.style.setProperty("visibility", "visible", "important")
+    grabbutton.style.setProperty("opacity", "1", "important")
+    setTimeout(() => {
+        if (kmtBoot.getProps().page.linksEmbedded == undefined) {
+            // since it's a daft SPA this doesn't actually trigger
+            grabbutton.innerHTML = "Click me to reload before you can grab GPX"
+            grabbutton.addEventListener("click", function() {
+                location.reload()
+            })
+        } else {
+            // since it's a daft SPA this stays past its welcome
+            grabbutton.innerHTML = "Grab GPX"
+            grabbutton.addEventListener("click", function() {
+                const coords = kmtBoot.getProps().page.linksEmbedded.tour.coords()
+                downloadGpx(`${new Date().toISOString()}.gpx`, jsonToGpx(coords))
+            })
+        }
+        document.body.prepend(grabbutton)
+    }, 1000)
+})()
---
+++ readme.md
@@ -0,0 +1,7 @@
+# userscript for grabbing gpx off komoot
+
+1) install tampermonkey or whatever in your browser
+2) install this script
+3) go to a route you want to download, click "edit this route"
+4) reload the page (important!) with ctrl+r or something else
+5) click "grab GPX"
